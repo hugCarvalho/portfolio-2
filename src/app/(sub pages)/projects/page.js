1cf9e9dab6d86fc84cd7e3ff43a1b69d8f8687e8
Hugo Carvalho <webnoico1978@gmail.com>
2025-03-31 10:33:07 +0200

    added projects page

---
+++ src/app/(sub pages)/projects/page.js
@@ -0,0 +1,21 @@
+import ProjectList from "@/components/projects/ProjectList";
+
+export const metadata = {
+  title: "Projects",
+};
+
+export default function ProjectsPage() {
+  return (
+    <>
+      {/* <Image
+        src={bg}
+        alt="Next.js Portfolio website's about page background image"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
+        priority
+        sizes="100vw"
+      /> */}
+
+      <ProjectList/>
+    </>
+  );
+}
---
+++ src/components/projects/ProjectList.jsx
@@ -0,0 +1,45 @@
+"use client";
+import { privateProjectsData, professionalProjectsData } from "@/app/config";
+import { motion } from "framer-motion";
+import ProjectListLayout from "./ProjectListLayout";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.4,
+      delayChildren: .51,
+    },
+  },
+};
+
+const ProjectList = () => {
+  return (
+  <>
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
+      <h1 className="pb-16 text-5xl">Projects</h1>
+    </motion.div>
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center"
+    >
+      {professionalProjectsData.map((project, index) => {
+        return <ProjectListLayout key={index} {...project} />;
+      })}
+      {privateProjectsData.map((project, index) => {
+        return <ProjectListLayout key={index} {...project} />;
+      })}
+    </motion.div>
+
+    </>
+  );
+};
+
+export default ProjectList;
