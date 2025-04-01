278c9f3493d1823395821cdc570d791c8532a779
Hugo Carvalho <webnoico1978@gmail.com>
2025-04-01 12:13:07 +0200

    about page

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -1,4 +1,4 @@
-import AboutDetails from "@/components/about";
+import AboutDetails from "@/components/about/About";
 
 export const metadata = {
   title: "About",
@@ -14,6 +14,17 @@ export default function About() {
         alt="Next.js Portfolio website's about page background image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
       /> */}
+
+      {/* <div className="relative w-full flex flex-col items-center justify-center">
+        <div className=" flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="font-bold  text-6xl xs:text-7xl sm:text-8xl  lg:text-9xl text-accent">
+            Hugo Carvalho
+          </h1>
+          <p className="font-light text-foreground text-lg">
+            Text...
+          </p>
+        </div>
+      </div> */}
 
       <AboutDetails />
     </>
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,10 +7,12 @@ const AboutDetails = () => {
       <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
         <ItemLayout className=" col-span-full lg:col-span-8 row-span-2 flex-col items-start">
           <h2 className="text-xl md:text-2xl text-left w-full capitalize">
-            About Me Title
+            Hugo Carvalho
           </h2>
           <p className="font-light text-xs sm:text-sm md:text-base">
-            Description section
+          A highly motivated and adaptable self-taught programmer with five years of practical experience building responsive and efficient applications within the React ecosystem. I possess a proven ability to quickly learn and master new technologies, driven by a proactive approach to continuous learning and a passion for staying at the forefront of frontend development.
+
+          I am a strong collaborator, able to work effectively in team environments, communicate clearly, and contribute to shared goals. My detail-oriented approach ensures the delivery of high-quality solutions, with a strong emphasis on creating exceptional user experiences. This dedication, coupled with a genuine enthusiasm for programming, has been the driving force behind my successful career change.
           </p>
         </ItemLayout>
 
