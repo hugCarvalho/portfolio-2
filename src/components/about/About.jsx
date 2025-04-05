8ac57cfd2d1f8135451eed0bba382aee4eab02b2
Hugo Carvalho <webnoico1978@gmail.com>
2025-04-05 14:36:59 +0200

    updated formating about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,9 +10,16 @@ const AboutDetails = () => {
             Hugo Carvalho
           </h2>
           <p className="font-light text-xs sm:text-sm md:text-base">
-          A highly motivated and adaptable self-taught programmer with five years of practical experience building responsive and efficient applications within the React ecosystem. I possess a proven ability to quickly learn and master new technologies, driven by a proactive approach to continuous learning and a passion for staying at the forefront of frontend development.
-
-          I am a strong collaborator, able to work effectively in team environments, communicate clearly, and contribute to shared goals. My detail-oriented approach ensures the delivery of high-quality solutions, with a strong emphasis on creating exceptional user experiences. This dedication, coupled with a genuine enthusiasm for programming, has been the driving force behind my successful career change.
+            A highly motivated and adaptable self-taught programmer with five years of practical experience building responsive and efficient applications within the React ecosystem.
+            <span className="inline-block">
+            I possess a proven ability to quickly learn and master new technologies, driven by a proactive approach to continuous learning and a passion for staying at the forefront of frontend development.
+            </span>
+            <span className="inline-block">
+              I am a strong collaborator, able to work effectively in team environments, communicate clearly, and contribute to shared goals. My detail-oriented approach ensures the delivery of high-quality solutions, with a strong emphasis on creating exceptional user experiences.
+            </span>
+            <span className="inline-block">
+              This dedication, coupled with a genuine enthusiasm for programming, has been the driving force behind my successful career change.
+            </span>
           </p>
         </ItemLayout>
 
